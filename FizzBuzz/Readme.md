8ea9bf5087d4af7091f2b4dbb1b5cec62cc1c0b5
Drew <drew90@vt.edu>
2021-01-02 20:35:33 -0800

    Added FizzBuzz example for chapter 8 of Rust book.

---
+++ FizzBuzz/Readme.md
@@ -0,0 +1,3 @@
+# FizzBuzz
+
+Just in case I ever get interviewed for Rust 😉. But actually because I was reading the strings chapter of the Rust book and thought I should do fizzbuzz.
---
+++ FizzBuzz/src/main.rs
@@ -0,0 +1,74 @@
+use std::io;
+
+fn main() {
+    let max_val: u32 = {
+        println!("Please input the largest number to FizzBuzz to.");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        input
+            .trim()
+            .parse()
+            .expect("Need to put in a valid u32 value.")
+    };
+
+    println!("\nSimple Version:");
+    with_ifel(max_val);
+
+    println!("\nWith Strings:");
+    with_String(max_val);
+
+    println!("\nWith strs:");
+    with_str(max_val);
+}
+
+fn with_ifel(max_val: u32) {
+    for i in 1..=max_val {
+        if i % 15 == 0 {
+            println!("FizzBuzz");
+        } else if i % 3 == 0 {
+            println!("Fizz");
+        } else if i % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", i);
+        }
+    }
+}
+
+fn with_String(max_val: u32) {
+    let mut fb = String::new();
+
+    for i in 1..=max_val {
+        if i % 3 == 0 {
+            fb += "Fizz";
+        }
+        if i % 5 == 0 {
+            fb += "Buzz";
+        }
+        if fb.is_empty() {
+            fb = i.to_string();
+        }
+        println!("{}", fb);
+        fb.clear();
+    }
+}
+
+fn with_str(max_val: u32) {
+    let fb = "FizzBuzz";
+
+    for i in 1..=max_val {
+        // Not better than using string above I guess, 
+        // but just playing around here
+        let mut target = &i.to_string()[..];
+        if i % 15 == 0 {
+            target = &fb[..];
+        } else if i % 3 == 0 {
+            target = &fb[..4];
+        } else if i % 5 == 0 {
+            target = &fb[4..];
+        }
+        println!("{}", target);
+    }
+}
