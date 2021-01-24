24cf9f94ae13d8a457a5e2e9efc8b10d5ac97599
Drew <drew90@vt.edu>
2021-01-23 22:00:39 -0800

    Made a blinker for my Raspberry Pi 4 for a RGB LED.

--- RPiBlinkLed/src/main.rs
+++ RPiBlinkLed/src/main.rs
@@ -1,3 +1,30 @@
+use rust_gpiozero::*;
+use std::time;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let blue = LED::new(17);
+    let green = LED::new(27);
+    let red = LED::new(22);
+    blue.on();
+    green.on();
+    red.on();
+
+    let sleep_time = time::Duration::from_secs(2); 
+    loop{
+       blue.off(); 
+       green.on();
+       red.on();
+       thread::sleep(sleep_time);
+
+       blue.on(); 
+       green.off();
+       red.on();
+       thread::sleep(sleep_time);
+
+       blue.on(); 
+       green.on();
+       red.off();
+       thread::sleep(sleep_time);
+    }
 }
